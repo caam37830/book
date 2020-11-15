775c1478c6713084a034feba95796d6b7d5043f1
Brad Nelson <bjnelson@stanford.edu>
2020-11-15 15:21:16 -0600

    update source code

--- 00_python/mymod.py
+++ 00_python/mymod.py
@@ -7,3 +7,17 @@ def plus1(x):
     returns x + 1
     """
     return x + 1
+
+
+class myclass:
+    """
+    simple class in a simple module
+    """
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+
+if __name__ == '__main__':
+    print("mymod.py is being run as a script!")
+    print("plus1(1) = {}".format(plus1(1)))
